a0563ad9d3d4f5032e880f5c1234e2c99b04aba7
Kang Ming Chua <kambing860210@gmail.com>
2017-08-22 02:00:17 +0800

    fix image path

--- src/helpers/getPoster.jsx
+++ src/helpers/getPoster.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export default (posterPath) => {
   if (posterPath) {
-    return (<img src={`http://image.tmdb.org/t/p/w185${posterPath}`} alt={posterPath} />);
+    return (<img src={`//image.tmdb.org/t/p/w185${posterPath}`} alt={posterPath} />);
   }
   return (<div className="empty-image" />);
 };
